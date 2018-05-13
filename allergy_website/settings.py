b503abd4f2038335005efd53c913c1f9f8b12c67
Drew Stimson <drewstimson@Drews-MacBook-Air.local>
2018-05-13 01:15:42 -0400

    changes for going into production

--- allergy_website/settings.py
+++ allergy_website/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
